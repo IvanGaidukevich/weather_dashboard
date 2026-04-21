bd26449830ca816a6c380db9b56af721aa844f69
vania_delay <gaydu7@gmail.com>
2026-04-21 20:25:43 +0300

    add server

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from callbacks import register_callbacks
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = 'Погодный дашборд'
 app.layout = create_layout()
+server = app.server
 
 
 register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
